a10c5f344e3bd1e821b27dd952a0278991ab333f
CoKeFish <37970518+CoKeFish@users.noreply.github.com>
2022-10-04 18:56:23 -0500

    add toastify

---
+++ templatemo_570_chain_app_dev/templatemo_570_chain_app_dev/assets/js/src/showMessage.js
@@ -0,0 +1,18 @@
+
+export function showMessage(message, styleBg) {
+
+    Toastify({
+        text: message,
+        duration: 5000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background: styleBg,
+        },
+        onClick: function () { }, // Callback after click
+    }).showToast();
+}
--- templatemo_570_chain_app_dev/templatemo_570_chain_app_dev/assets/js/src/singUpForm.js
+++ templatemo_570_chain_app_dev/templatemo_570_chain_app_dev/assets/js/src/singUpForm.js
@@ -2,41 +2,52 @@
 
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 import { auth } from "./firebase.js"; // firebase auth
+import { showMessage } from "./showMessage.js"; // show message
 
 //Get form
 const singUpForm = document.querySelector("#signup-form");
 
 singUpForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const email = singUpForm["signup-email"].value;
-  const password = singUpForm["signup-password"].value;
+    const email = singUpForm["signup-email"].value;
+    const password = singUpForm["signup-password"].value;
 
-  //console.log(email, password);
+    //console.log(email, password);
 
-  try {
-    const userCredentials = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    console.log(userCredentials);
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+        console.log(userCredentials);
 
-    const modal_id = document.querySelector(".popupContainer");
-    //console.log(modal_id);
-    $("#lean_overlay").fadeOut(200);
+        showMessage(userCredentials.user.email + " ha sido creado", "linear-gradient(to right, #00b09b, #96c93d)");
 
-    $(modal_id).css({
-      display: "none",
-    });
-  } catch (error) {
-    console.log(error.message);
-    console.log(error.code);
+        const modal_id = document.querySelector(".popupContainer");
+        //console.log(modal_id);
+        $("#lean_overlay").fadeOut(200);
 
-    if (error.code === "auth/invalid-email") {
-      alert("Invalid email address");
+        $(modal_id).css({
+            display: "none",
+        });
+
+        
+    } catch (error) {
+        console.log(error.message);
+        console.log(error.code);
+
+        if (error.code === "auth/invalid-email") {
+            showMessage("Invalid email address", "linear-gradient(to right, #00b09b, #96c93d)")
+        } else if (error.code === "auth/weak-password") {
+            showMessage("Password is too weak")
+        } else if (error.code === "auth/email-already-in-use", "linear-gradient(to right, #00b09b, #96c93d)") {
+            showMessage("Email already in use", "linear-gradient(to right, #00b09b, #96c93d)")
+        } else if (error.code) {
+            showMessage("Something went wrong", "linear-gradient(to right, #00b09b, #96c93d)");
+        }
     }
-  }
 });
 
 //console.log(singUpForm);
