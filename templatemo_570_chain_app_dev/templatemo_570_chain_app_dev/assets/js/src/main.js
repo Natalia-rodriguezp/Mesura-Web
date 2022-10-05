52f1325c68fde259afab2e8f1f676aa755f2800a
CoKeFish <37970518+CoKeFish@users.noreply.github.com>
2022-10-04 20:02:10 -0500

    po

--- templatemo_570_chain_app_dev/templatemo_570_chain_app_dev/assets/js/src/main.js
+++ templatemo_570_chain_app_dev/templatemo_570_chain_app_dev/assets/js/src/main.js
@@ -1,4 +1,21 @@
-import './firebase.js';
-import './singUpForm.js';
 
-console.log("Hello world!");
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+
+import { auth } from './firebase.js';
+
+
+import './singUpForm.js';
+import './logout.js';
+import './loginCheck.js'
+
+
+
+onAuthStateChanged(auth, async (user) => {
+    console.log(user)
+    if (user) {
+
+    }
+    else {
+
+    }
+})
