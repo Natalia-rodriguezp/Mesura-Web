de8eeed01d98cbf4ba0f99b0cf04edfc28708c02
CoKeFish <37970518+CoKeFish@users.noreply.github.com>
2022-10-05 09:27:14 -0500

    Update main.js

--- templatemo_570_chain_app_dev/templatemo_570_chain_app_dev/assets/js/src/main.js
+++ templatemo_570_chain_app_dev/templatemo_570_chain_app_dev/assets/js/src/main.js
@@ -10,16 +10,6 @@ import './googleLogin.js';
 import './FacebookLogin.js';
 import './logout.js';
 
-const user = auth.currentUser;
-
-if (user) {
-  // User is signed in, see docs for a list of available properties
-  // https://firebase.google.com/docs/reference/js/firebase.User
-  // ...
-  console.log('User is signed in');
-} else {
-  console.log('User is not signed in');
-}
 
 onAuthStateChanged(auth, async (user) => {
     console.log(user)
